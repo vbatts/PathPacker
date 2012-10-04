68ade68d4e9f732d8b485aa86d4d1c6673942986
Vincent Batts <vbatts@redhat.com>
2012-10-04 15:43:26 -0400

    now it reads a file, to have a List<String> from a file.

--- src/main/java/com/hashbangbash/trie/App.java
+++ src/main/java/com/hashbangbash/trie/App.java
@@ -1,14 +1,25 @@
 package com.hashbangbash.trie;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
+
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
         FileInputStream fis;
         DataInputStream in;
+        BufferedReader br;
+
+        String content;
+        List<String> contentList;
 
         for (String arg : args) {
             try {
@@ -20,6 +31,21 @@ public class App {
                 System.out.printf("ERROR: [%s] %s\n", arg, t);
                 continue;
             }
+
+            in = new DataInputStream(fis);
+            br = new BufferedReader(new InputStreamReader(in));
+            contentList = new ArrayList<String>();
+
+            try {
+            while ((content = br.readLine()) != null) {
+                contentList.add(content);
+            }
+            } catch (IOException ex) {
+                System.out.printf("ERROR: [%s] - %s\n", arg, ex);
+                continue;
+            }
+
+            System.out.println(contentList.toString());
         }
     }
 }
