dcc9c50655b437c3639026b7ed10be6449848186
Vincent Batts <vbatts@redhat.com>
2012-10-31 21:46:59 -0400

    NodeContext tests

--- src/test/java/com/redhat/trie/TestNodeContext.java
+++ src/test/java/com/redhat/trie/TestNodeContext.java
@@ -1,6 +1,5 @@
 package com.redhat.trie;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -9,9 +8,25 @@ import org.junit.Test;
 public class TestNodeContext {
 
     @Test
-    public void testNew(){
+    public void testNew1() {
         NodeContext nc = new NodeContext();
         assertNotNull(nc);
+    }
+
+    @Test
+    public void testNew2() {
+        NodeContext nc = new NodeContext(1);
+        assertNotNull(nc);
+    }
+
+    @Test
+    public void testCounter() {
+        NodeContext nc = new NodeContext();
+        assertEquals(nc.getId(), 0);
+
+        long next = nc.nextId();
+        assertEquals(nc.getId(), 1);
+        assertEquals(nc.getId(), (next + 1));
     }
 }
 
