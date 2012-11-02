b36fddf91da8f95b4d46d128644dfaa66407c2ca
Vincent Batts <vbatts@redhat.com>
2012-11-02 09:50:51 -0400

    moving around the test data files, for unit testing

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-java -cp ./lib/bcprov-jdk16-1.46.jar:$(ls -rt ./dist/*jar | tail -1) com.hashbangbash.trie.App ./data/test-certv3.pem
+java \
+	-cp ./lib/bcprov-jdk16-1.46.jar:$(ls -rt ./target/*jar | tail -1) \
+	com.hashbangbash.trie.App \
+	./src/test/resources/test-certv3.pem
 
